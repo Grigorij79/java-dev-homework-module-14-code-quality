97935a791da245bcf0eb4e65c813cac458ad417c
KovGr <KovGrDm@outlook.com>
2024-04-13 23:06:20 +0300

    refactoring2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-       Game game = new Game();
-       game.startGame();
-
+        Game game = new Game();
+        game.startGame();
     }
 }
